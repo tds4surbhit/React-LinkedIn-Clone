ef30a414a62373a8b66b99ac52e082cefe331f33
tds4surbhit <surbhit.4zeracing@gmail.com>
2022-03-02 02:32:52 +0530

    Adding Header Design Components

---
+++ src/HeaderOptions.js
@@ -0,0 +1,16 @@
+import { Icon } from '@mui/material'
+import React from 'react'
+import './HeaderOption.css'
+
+function HeaderOptions({ Icon , title }) {
+  return (
+    <div className='HeaderOption'>
+        {Icon && <Icon className='headerOption__icon' />}
+        <h3 className='headerOption__title'>{title}</h3>
+    
+    
+    </div>
+  )
+}
+
+export default HeaderOptions
